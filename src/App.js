10608df77af50dbf48b621305086a96beb3b4e4f
mathlete01 <msallin@gmail.com>
2021-12-18 19:18:36 -0800

    Set up react router dom

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import { Link } from "react-router-dom";
+import Main from "./components/Main.js";
 
 function App() {
 	return (
@@ -24,37 +26,16 @@ function App() {
 						<logo></logo>
 					</Navbar.Brand>
 					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#features">Contact</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
+						<Nav.Link as={Link} to="/home">
+							Home
+						</Nav.Link>
+						<Nav.Link as={Link} to="/contact">
+							Contact
+						</Nav.Link>
 					</Nav>
 				</Container>
 			</Navbar>
-			<Container>
-				<Row>
-					<Image src={hero} fluid />
-				</Row>
-				<Row>
-					<p>
-						On the other hand, we denounce with righteous indignation and
-						dislike men who are so beguiled and demoralized by the charms of
-						pleasure of the moment, so blinded by desire, that they cannot
-						foresee the pain and trouble that are bound to ensue; and equal
-						blame belongs to those who fail in their duty through weakness of
-						will, which is the same as saying through shrinking from toil and
-						pain. These cases are perfectly simple and easy to distinguish. In a
-						free hour, when our power of choice is untrammelled and when nothing
-						prevents our being able to do what we like best, every pleasure is
-						to be welcomed and every pain avoided. But in certain circumstances
-						and owing to the claims of duty or the obligations of business it
-						will frequently occur that pleasures have to be repudiated and
-						annoyances accepted. The wise man therefore always holds in these
-						matters to this principle of selection: he rejects pleasures to
-						secure other greater pleasures, or else he endures pains to avoid
-						worse pains.
-					</p>
-				</Row>
-			</Container>
+			<Main />
 		</div>
 	);
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Contact() {
+	return (
+		<div className="Contact">
+			<Container>
+				<Row>
+					<p>I am the contact page.</p>
+				</Row>
+			</Container>
+		</div>
+	);
+}
+
+export default Contact;
